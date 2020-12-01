03ffda6045177ae6972872eac521f2e178942985
Steven Vch <unstatik@staremax.com>
2020-11-30 20:32:36 -0500

    added more examples with interact game log data

--- example.py
+++ example.py
@@ -11,6 +11,8 @@ import aiohttp
 from ms.base import MSRPCChannel
 from ms.rpc import Lobby
 import ms.protocol_pb2 as pb
+from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import MessageToDict
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
@@ -28,17 +30,28 @@ async def main():
     parser.add_option('-p', '--password',
                       type='string',
                       help='Your account password.')
+    parser.add_option('-l', '--log',
+                      type='string',
+                      help='Your log UUID for load.')
 
     opts, _ = parser.parse_args()
     username = opts.username
     password = opts.password
+    log_uuid = opts.log
+
     if not username or not password:
         parser.error('Username or password cant be empty')
 
     lobby, channel = await connect()
     await login(lobby, username, password)
-    game_logs = await load_game_logs(lobby)
-    logging.info('Found {} records'.format(len(game_logs)))
+
+    if not log_uuid:
+    	game_logs = await load_game_logs(lobby)
+    	logging.info('Found {} records'.format(len(game_logs)))
+
+    if log_uuid:
+    	game_log = await load_and_process_game_log(lobby, log_uuid)
+    	logging.info('game {} result : \n{}'.format(game_log.head.uuid,game_log.head.result))
 
     await channel.close()
 
@@ -109,6 +122,58 @@ async def load_game_logs(lobby):
     records.extend([r.uuid for r in res.record_list])
 
     return records
+
+async def load_and_process_game_log(lobby, uuid):
+    logging.info('Loading game log')
+
+    req = pb.ReqGameRecord()
+    req.game_uuid = uuid
+    res = await lobby.fetch_game_record(req)
+
+    record_wrapper = pb.Wrapper()
+    record_wrapper.ParseFromString(res.data)
+
+    game_details = pb.GameDetailRecords()
+    game_details.ParseFromString(record_wrapper.data)
+
+    game_records_count = len(game_details.records)
+    logging.info('Found {} game records'.format(game_records_count))
+
+    round_record_wrapper = pb.Wrapper()
+    is_show_new_round_record = False
+    is_show_discard_tile = False
+    is_show_deal_tile = False
+
+    for i in range(0, game_records_count):
+        round_record_wrapper.ParseFromString(game_details.records[i])
+
+        if round_record_wrapper.name == '.lq.RecordNewRound' and not is_show_new_round_record:
+                logging.info('Found record type = {}'.format(round_record_wrapper.name))
+                round = pb.RecordNewRound()
+                round.ParseFromString(round_record_wrapper.data)
+                print_data_as_json(round,'RecordNewRound')
+                is_show_new_round_record = True
+
+        if round_record_wrapper.name == '.lq.RecordDiscardTile' and not is_show_discard_tile:
+                logging.info('Found record type = {}'.format(round_record_wrapper.name))
+                discard_tile = pb.RecordDiscardTile()
+                discard_tile.ParseFromString(round_record_wrapper.data)
+                print_data_as_json(discard_tile,'RecordDiscardTile')
+                is_show_discard_tile = True
+
+        if round_record_wrapper.name == '.lq.RecordDealTile' and not is_show_deal_tile:
+                logging.info('Found record type = {}'.format(round_record_wrapper.name))
+                deal_tile = pb.RecordDealTile()
+                deal_tile.ParseFromString(round_record_wrapper.data)
+                print_data_as_json(deal_tile,'RecordDealTile')
+                is_show_deal_tile = True
+
+    return res
+
+def print_data_as_json(data, type):
+    json = MessageToJson(data)
+    logging.info('{} json {}'.format(type,json))
+
 
 
 if __name__ == '__main__':
